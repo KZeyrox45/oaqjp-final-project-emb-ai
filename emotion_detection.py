aa648de06653d3b64097dbb58315cd8815485ba5
KZeyrox45 <hieunguyenst468@gmail.com>
2026-04-17 23:25:54 -0400

    Format response and return a set of emotions with their score and the dominant emotion

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 def emotion_detector(text_to_analyze):
     # URL of the emotion detector service
@@ -13,5 +14,27 @@ def emotion_detector(text_to_analyze):
     # Send a POST request to the API with the text and headers
     response = requests.post(url, json=myobj, headers=headers)
 
+    # Parsing the JSON response from the API
+    formatted_response = json.loads(response.text)
+
+    # Extract anger, disgust, fear, joy and sadness, along with their scores
+    emotions = formatted_response['emotionPredictions'][0]['emotion']
+    
+    anger = emotions['anger']
+    disgust = emotions['disgust']
+    fear = emotions['fear']
+    joy = emotions['joy']
+    sadness = emotions['sadness']
+
+    # Find the dominant_emotion which has the highest score in emotions
+    dominant_emotion = max(emotions, key=emotions.get)
+
     # Return the response text from the API
-    return response.text
+    return {
+        'anger': anger,
+        'disgust': disgust,
+        'fear': fear,
+        'joy': joy,
+        'sadness': sadness,
+        'dominant_emotion': dominant_emotion
+    }
